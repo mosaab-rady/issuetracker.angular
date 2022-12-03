b0f85bca2403951004ba9aac951e0064d8665a74
mosaab-rady <mosp.rady@gmail.com>
2022-12-03 23:06:20 +0200

    add auth service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginDto } from 'src/app/shared/Dtos/Users/LoginDto';
+import { UserDto } from 'src/app/shared/Dtos/Users/UserDto';
+import { Url } from 'src/app/shared/Url';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  USER: UserDto | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  Login(LoginDto: LoginDto): Observable<UserDto> {
+    return this.http.post<UserDto>(`${Url}/api/account/login`, LoginDto, {
+      withCredentials: true,
+    });
+  }
+}
