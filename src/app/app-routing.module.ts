54d59dc8044612700b7e5a4eb22f4109edba5fc6
mosaab-rady <mosp.rady@gmail.com>
2022-12-04 23:25:08 +0200

    add router to signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/Components/login/login.component';
+import { SignupComponent } from './auth/Components/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'login', title: 'login | Issue Tracker', component: LoginComponent },
+  { path: 'login', title: 'Login | Issue Tracker', component: LoginComponent },
+  {
+    path: 'signup',
+    title: 'Sign Up | Issue Tracker',
+    component: SignupComponent,
+  },
 ];
 
 @NgModule({
