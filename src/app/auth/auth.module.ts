34bb0da5bf8fb5d22f664b3ed9fbcf8b669b44e6
mosaab-rady <mosp.rady@gmail.com>
2022-12-04 23:26:34 +0200

    add signup component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { LoginComponent } from './Components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from '../app-routing.module';
+import { SignupComponent } from './Components/signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     AppRoutingModule,
