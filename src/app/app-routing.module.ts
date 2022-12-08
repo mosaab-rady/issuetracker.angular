db862da0cbf73f7c7d0659cfc556acdeeaca6ef7
mosaab-rady <mosp.rady@gmail.com>
2022-12-08 22:57:29 +0200

    add routing for email-confirmed and confirm-email components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfirmEmailComponent } from './auth/Components/confirm-email/confirm-email.component';
+import { EmailConfirmedComponent } from './auth/Components/email-confirmed/email-confirmed.component';
 import { LoginComponent } from './auth/Components/login/login.component';
 import { SignupComponent } from './auth/Components/signup/signup.component';
 
@@ -9,6 +11,16 @@ const routes: Routes = [
     path: 'signup',
     title: 'Sign Up | Issue Tracker',
     component: SignupComponent,
+  },
+  {
+    path: 'confirm-email',
+    title: 'Confirm Email | Issue Tracker',
+    component: ConfirmEmailComponent,
+  },
+  {
+    path: 'email-confirmed',
+    title: 'Email Confirmed | Issue Tracker',
+    component: EmailConfirmedComponent,
   },
 ];
 
