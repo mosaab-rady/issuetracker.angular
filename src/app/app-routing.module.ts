53c912a96cf20ee5310e11c6420f5a13de2d0de7
mosaab-rady <mosp.rady@gmail.com>
2022-12-09 20:58:07 +0200

    add routing for layout and notfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { ConfirmEmailComponent } from './auth/Components/confirm-email/confirm-e
 import { EmailConfirmedComponent } from './auth/Components/email-confirmed/email-confirmed.component';
 import { LoginComponent } from './auth/Components/login/login.component';
 import { SignupComponent } from './auth/Components/signup/signup.component';
+import { AuthGuard } from './auth/services/auth.guard';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+// import { AuthGuard } from './auth/services/auth.guard';
 
 const routes: Routes = [
   { path: 'login', title: 'Login | Issue Tracker', component: LoginComponent },
@@ -21,6 +25,16 @@ const routes: Routes = [
     path: 'email-confirmed',
     title: 'Email Confirmed | Issue Tracker',
     component: EmailConfirmedComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Not Found | Issue Tracker',
   },
 ];
 
