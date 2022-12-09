0320d6f214dde3fa95397719165bd503bb3ba71f
mosaab-rady <mosp.rady@gmail.com>
2022-12-09 21:01:44 +0200

    add layout component to declarations

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavbarComponent, LayoutComponent, NotFoundComponent],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
