1a7c49ab735ecc8ffe675e514f30a35273eb3af2
mosaab-rady <mosp.rady@gmail.com>
2022-12-09 21:01:11 +0200

    use enviroment

--- src/app/auth/services/check-email.service.ts
+++ src/app/auth/services/check-email.service.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
 import { map, Observable } from 'rxjs';
-import { Url } from 'src/app/shared/Url';
+import { enviroment } from 'src/enviroments/enviroment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckEmailService implements AsyncValidator {
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
-    return this.http.get<boolean>(`${Url}/api/account/isEmailUsed?email=${control.value}`).pipe(
-      map(isUsed => (isUsed ? { usedEmail: true } : null))
-    );
+    return this.http
+      .get<boolean>(
+        `${enviroment.apiUrl}/api/account/isEmailUsed?email=${control.value}`
+      )
+      .pipe(map((isUsed) => (isUsed ? { usedEmail: true } : null)));
   }
-
 }
