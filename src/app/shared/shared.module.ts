515a2d9a411a2b77acb3a3b24d1567cfee08760a
mosaab-rady <mosp.rady@gmail.com>
2022-12-11 23:11:48 +0200

    add sidebar and icons-sidebar componenets

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { IconsSidebarComponent } from './components/icons-sidebar/icons-sidebar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, LayoutComponent, NotFoundComponent],
+  declarations: [NavbarComponent, LayoutComponent, NotFoundComponent, SidebarComponent, IconsSidebarComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
