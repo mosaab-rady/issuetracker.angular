6245cc1cac8e4eb534952bb97135b309737e04e0
mosaab-rady <mosp.rady@gmail.com>
2022-12-12 23:38:13 +0200

    remove layout components from sahred module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { IconsSidebarComponent } from './components/icons-sidebar/icons-sidebar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, LayoutComponent, NotFoundComponent, SidebarComponent, IconsSidebarComponent],
+  declarations: [NotFoundComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
