a18e9e689aaebc785951dde6886a1ca308fab963
mosaab-rady <mosp.rady@gmail.com>
2022-12-12 23:35:55 +0200

    add projects module

---
+++ src/app/projects/projects.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectsRoutingModule } from './projects-routing.module';
+import { AllProjectsComponent } from './pages/all-projects/all-projects.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+
+@NgModule({
+  declarations: [AllProjectsComponent, ProjectDetailComponent],
+  imports: [CommonModule, ProjectsRoutingModule],
+})
+export class ProjectsModule {}
