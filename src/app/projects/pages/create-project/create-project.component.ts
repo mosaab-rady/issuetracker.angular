0a16b50585d04d5a9ac10af486bead44c8780c4f
mosaab-rady <mosp.rady@gmail.com>
2022-12-14 23:03:27 +0200

    add create project component

---
+++ src/app/projects/pages/create-project/create-project.component.ts
@@ -0,0 +1,82 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CreateProjectDto } from '../../Dtos/CreateProjectDto';
+import { ProjectsService } from '../../services/projects.service';
+
+interface CreateProject {
+  Name: FormControl<string>;
+  StartDate: FormControl<Date>;
+  TargetEndDate: FormControl<Date>;
+}
+
+@Component({
+  selector: 'app-create-project',
+  templateUrl: './create-project.component.html',
+  styleUrls: ['./create-project.component.css'],
+})
+export class CreateProjectComponent {
+  /**
+   *
+   */
+  constructor(
+    private projectsService: ProjectsService,
+    private router: Router,
+    public location: Location
+  ) {}
+
+  createProjectForm = new FormGroup<CreateProject>({
+    Name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    StartDate: new FormControl(),
+    TargetEndDate: new FormControl(),
+  });
+
+  Submit(): void {
+    console.log(this.createProjectForm);
+
+    if (this.createProjectForm.valid) {
+      document
+        .getElementById('createProjectBtn')
+        ?.setAttribute('value', 'Please Wait....');
+
+      document
+        .getElementById('createProjectBtn')
+        ?.setAttribute('disabled', 'true');
+
+      this.Create();
+    }
+  }
+
+  Create(): void {
+    const project: CreateProjectDto = {
+      name: this.createProjectForm.value.Name!,
+      startDate: this.createProjectForm.value.StartDate!,
+      targetEndDate: this.createProjectForm.value.TargetEndDate!,
+    };
+
+    this.projectsService.CreateProject(project).subscribe({
+      error: (e) => {
+        // console.log('error', e);
+        this.createProjectForm.setErrors({
+          0: e.error.detail || 'somthing went wrong try again later',
+        });
+
+        document
+          .getElementById('createProjectBtn')
+          ?.setAttribute('value', 'Create Project');
+
+        document
+          .getElementById('createProjectBtn')
+          ?.removeAttribute('disabled');
+      },
+      complete: () => {
+        // console.log('complete');
+        this.router.navigate(['/projects']);
+      },
+    });
+  }
+}
