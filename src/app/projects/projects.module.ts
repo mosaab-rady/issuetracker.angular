933e319bdeb3cd6c852cf27ce5e3acfa5de72b0e
mosaab-rady <mosp.rady@gmail.com>
2022-12-14 23:01:36 +0200

    add declaration for projectCar and createProject

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { AllProjectsComponent } from './pages/all-projects/all-projects.component';
 import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { CreateProjectComponent } from './pages/create-project/create-project.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AllProjectsComponent, ProjectDetailComponent],
-  imports: [CommonModule, ProjectsRoutingModule],
+  declarations: [
+    AllProjectsComponent,
+    ProjectDetailComponent,
+    ProjectCardComponent,
+    CreateProjectComponent,
+  ],
+  imports: [CommonModule, ProjectsRoutingModule, ReactiveFormsModule],
 })
 export class ProjectsModule {}
