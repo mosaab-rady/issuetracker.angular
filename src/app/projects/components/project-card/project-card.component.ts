517b1468ff3334df9c6acd68afef6f7f58986dd0
mosaab-rady <mosp.rady@gmail.com>
2022-12-15 22:53:56 +0200

    add delete project

--- src/app/projects/components/project-card/project-card.component.ts
+++ src/app/projects/components/project-card/project-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ProjectDto } from '../../Dtos/ProjectDto';
+import { ProjectsService } from '../../services/projects.service';
 
 @Component({
   selector: 'app-project-card',
@@ -8,4 +9,28 @@ import { ProjectDto } from '../../Dtos/ProjectDto';
 })
 export class ProjectCardComponent {
   @Input() project!: ProjectDto;
+
+  /**
+   *
+   */
+  constructor(private projectsService: ProjectsService) {}
+
+  deleteProject(id: string): void {
+    document.getElementById(`confirm-${id}`)?.setAttribute('disabled', 'true');
+    document.getElementById(`confirm-${id}`)?.setAttribute('value', 'Wait....');
+
+    this.projectsService.DeleteProject(id).subscribe({
+      complete: () => {
+        location.reload();
+      },
+    });
+  }
+
+  showConfirmDelete(id: string): void {
+    document.getElementById(id)!.style.display = 'block';
+  }
+
+  hideConfirmDelete(id: string): void {
+    document.getElementById(id)!.style.display = 'none';
+  }
 }
