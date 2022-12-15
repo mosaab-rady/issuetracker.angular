2c2e627f753aa3032c1f52120202a3e12400835c
mosaab-rady <mosp.rady@gmail.com>
2022-12-15 22:54:51 +0200

    add get, update, delete project methods

--- src/app/projects/services/projects.service.ts
+++ src/app/projects/services/projects.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { enviroment } from 'src/enviroments/enviroment';
 import { CreateProjectDto } from '../Dtos/CreateProjectDto';
 import { ProjectDto } from '../Dtos/ProjectDto';
+import { UpdateProjectDto } from '../Dtos/UpdateProjectDto';
 
 @Injectable({
   providedIn: 'root',
@@ -17,9 +18,35 @@ export class ProjectsService {
     });
   }
 
+  GetProject(id: string): Observable<ProjectDto> {
+    return this.http.get<ProjectDto>(
+      `${enviroment.apiUrl}/api/projects/${id}`,
+      {
+        withCredentials: true,
+      }
+    );
+  }
+
   CreateProject(project: CreateProjectDto): Observable<object> {
     return this.http.post(`${enviroment.apiUrl}/api/projects`, project, {
       withCredentials: true,
     });
+  }
+
+  DeleteProject(id: string): Observable<object> {
+    return this.http.delete(`${enviroment.apiUrl}/api/projects/${id}`, {
+      withCredentials: true,
+    });
+  }
+
+  UpdateProject(
+    updateProjectDto: UpdateProjectDto,
+    id: string
+  ): Observable<ProjectDto> {
+    return this.http.put<ProjectDto>(
+      `${enviroment.apiUrl}/api/projects/${id}`,
+      updateProjectDto,
+      { withCredentials: true }
+    );
   }
 }
