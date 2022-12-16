ab7124c38a70521b31d5fc2e52cfa5e0a20c68d3
mosaab-rady <mosp.rady@gmail.com>
2022-12-16 22:30:27 +0200

    export toastComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ToastComponent } from './components/toast/toast.component';
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, ToastComponent],
   imports: [CommonModule],
+  exports: [ToastComponent],
 })
 export class SharedModule {}
