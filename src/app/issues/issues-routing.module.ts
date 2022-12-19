4c2f9ebff77ff9cc2916050d9c8743071d5e9a52
mosaab-rady <mosp.rady@gmail.com>
2022-12-19 22:23:15 +0200

    add routing to new issue

--- src/app/issues/issues-routing.module.ts
+++ src/app/issues/issues-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllIssuesComponent } from './pages/all-issues/all-issues.component';
+import { CreateIssueComponent } from './pages/create-issue/create-issue.component';
 
 const routes: Routes = [
   {
+    path: 'new-issue',
+    title: 'Create Issue | Issue Tracker',
+    component: CreateIssueComponent,
+  },
+  {
     path: '',
+    title: 'All Issues',
     component: AllIssuesComponent,
   },
 ];
