41161d5b8383e01b149d6701090878abfe6970f4
mosaab-rady <mosp.rady@gmail.com>
2022-12-19 22:23:39 +0200

    add decleration to new issue

--- src/app/issues/issues.module.ts
+++ src/app/issues/issues.module.ts
@@ -4,16 +4,15 @@ import { CommonModule } from '@angular/common';
 import { IssuesRoutingModule } from './issues-routing.module';
 import { AllIssuesComponent } from './pages/all-issues/all-issues.component';
 import { IssuesTableComponent } from './components/issues-table/issues-table.component';
-
+import { CreateIssueComponent } from './pages/create-issue/create-issue.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AllIssuesComponent,
-    IssuesTableComponent
+    IssuesTableComponent,
+    CreateIssueComponent,
   ],
-  imports: [
-    CommonModule,
-    IssuesRoutingModule
-  ]
+  imports: [CommonModule, IssuesRoutingModule, ReactiveFormsModule],
 })
-export class IssuesModule { }
+export class IssuesModule {}
