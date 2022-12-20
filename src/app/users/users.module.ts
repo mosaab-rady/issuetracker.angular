ee1975b099f116ec507cc5936731f68f90e3c6a1
mosaab-rady <mosp.rady@gmail.com>
2022-12-20 21:16:19 +0200

    declare user row component

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { AllUsersComponent } from './pages/all-users/all-users.component';
 import { UsersTableComponent } from './components/users-table/users-table.component';
-
+import { UserRowComponent } from './components/user-row/user-row.component';
 
 @NgModule({
-  declarations: [
-    AllUsersComponent,
-    UsersTableComponent
-  ],
-  imports: [
-    CommonModule,
-    UsersRoutingModule
-  ]
+  declarations: [AllUsersComponent, UsersTableComponent, UserRowComponent],
+  imports: [CommonModule, UsersRoutingModule],
 })
-export class UsersModule { }
+export class UsersModule {}
