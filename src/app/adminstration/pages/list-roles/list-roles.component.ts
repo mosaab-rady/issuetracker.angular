3a529b7b5d75d8a3899dbfa840903f17125ffe84
mosaab-rady <mosp.rady@gmail.com>
2022-12-21 21:12:54 +0200

    add list roles page

---
+++ src/app/adminstration/pages/list-roles/list-roles.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RolesService } from '../../services/roles.service';
+import { RoleDto } from '../../Dtos/RoleDto';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-list-roles',
+  templateUrl: './list-roles.component.html',
+  styleUrls: ['./list-roles.component.css'],
+})
+export class ListRolesComponent {
+  roles!: Observable<RoleDto[]>;
+  /**
+   *
+   */
+  constructor(private rolesService: RolesService) {}
+
+  ngOnInit(): void {
+    this.roles = this.rolesService.GetAllRoles();
+  }
+
+  trackByFn(_index: number, role: RoleDto): string {
+    return role.id;
+  }
+}
