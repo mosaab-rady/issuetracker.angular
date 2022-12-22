619c1c79127308e39224890649e4aa38e91a252b
mosaab-rady <mosp.rady@gmail.com>
2022-12-22 23:17:40 +0200

    add declarations for edit Role components

--- src/app/adminstration/adminstration.module.ts
+++ src/app/adminstration/adminstration.module.ts
@@ -6,9 +6,19 @@ import { ListRolesComponent } from './pages/list-roles/list-roles.component';
 import { RoleCardComponent } from './components/role-card/role-card.component';
 import { CreateRoleComponent } from './pages/create-role/create-role.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EditRoleComponent } from './pages/edit-role/edit-role.component';
+import { UsersInRoleComponent } from './components/users-in-role/users-in-role.component';
+import { EditUsersInRoleComponent } from './components/edit-users-in-role/edit-users-in-role.component';
 
 @NgModule({
-  declarations: [ListRolesComponent, RoleCardComponent, CreateRoleComponent],
+  declarations: [
+    ListRolesComponent,
+    RoleCardComponent,
+    CreateRoleComponent,
+    EditRoleComponent,
+    UsersInRoleComponent,
+    EditUsersInRoleComponent,
+  ],
   imports: [CommonModule, AdminstrationRoutingModule, ReactiveFormsModule],
 })
 export class AdminstrationModule {}
